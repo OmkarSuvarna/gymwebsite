c39e49eeafe183cf84be20d6d3ddcc6f6e8f8b48
OmkarSuvarna <omkar.suvarna@outlook.com>
2023-06-12 11:55:06 +0100

    Basic Layout Desgin - Needs upgrading

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-'use strict';
+"use strict";
+
+let slideIndex = 0;
+
+let showSlides = function () {
+  let i, j;
+  let slides = document.getElementsByClassName("carousel-images");
+  let dots = document.getElementsByClassName("dot");
+
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+    console.log(slides[i].style);
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
+  }
+  for (j = 0; j < dots.length; j++) {
+    dots[j].className = dots[j].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+  setTimeout(showSlides, 10000);
+};
+
+showSlides();
